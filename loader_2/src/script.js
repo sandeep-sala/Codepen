0c4de042aff0564a8390eceb50d40279b617250c
sandeep sala <sandeepsala@outlook.com>
2020-03-29 15:19:28 +0530

    script added

---
+++ loader_2/src/script.js
@@ -0,0 +1,5 @@
+var  r  = document.createElement("div");
+r.className = 'ldr';
+r.innerHTML = '<div class="in one"></div><div class="in two"></div><div class="in three"></div>';
+var k = document.getElementsByTagName("body")[0]
+k.appendChild(r)
