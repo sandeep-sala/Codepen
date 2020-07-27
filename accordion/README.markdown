e9d867b972aca74d064497adb7d36ca6f83ebede
sandeep-sala <sandeepsala@outlook.com>
2020-07-27 11:40:59 +0530

    accordion added

---
+++ accordion/README.markdown
@@ -0,0 +1,5 @@
+# Accordion
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/sandeep-sala/pen/ExPrMOO](https://codepen.io/sandeep-sala/pen/ExPrMOO).
+
+
---
+++ accordion/dist/script.js
@@ -0,0 +1,9 @@
+function showhideSector(el) {
+    el.classList.toggle("active");
+    var sector = el.nextElementSibling;
+    if (sector.style.maxHeight) {
+        sector.style.maxHeight = null;
+    } else {
+        sector.style.maxHeight = sector.scrollHeight + "px";
+    } 
+}
